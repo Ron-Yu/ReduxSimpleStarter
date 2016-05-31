6502c4ddf07d3a881b95f879bfb648b23af5085e
Ron Yu <ronyu77@gmail.com>
2016-05-31 21:18:15 +0800

    Managing app state with Redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function() {
+    return (
+        [
+            {title: 'Book 1', pages: '101'},
+            {title: 'Book 2', pages: '39'},
+            {title: 'Book 3', pages: '71'},
+            {title: 'Book 4', pages: '52'}
+        ]
+    );
+}
